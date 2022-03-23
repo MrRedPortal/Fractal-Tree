ff972dfd0aa23b2954b9ea4281bfc84c05fb8f01
MrRedPortal <MrRedPortal@users.noreply.github.com>
2022-03-23 14:23:06 +1100

    first commit

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var angle =0;
+
+var slider;
+function setup() {
+  createCanvas(400, 400);
+  slider = createSlider(0,TWO_PI,PI/4,0.01);
+}
+
+function draw() {
+  background(51);
+  angle = slider.value();
+  var length = 100;
+  stroke(255);
+  translate(200,height);
+  branch(length);
+}
+
+function branch(length){
+  line(0, 0, 0, -length);
+  translate(0,-length);
+  
+  if(length>1){
+    push();
+    rotate(angle);
+    branch(length*0.67);
+    pop();
+    push();
+    rotate(-angle);
+    branch(length*0.67);
+    pop();
+  }
+}
